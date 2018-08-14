edc9ae28d104fb775537f9357ab2020088bcfbe7
intimate0305 <intimate0305@naver.com>
2018-08-14 23:14:52 +0900

    [UPDATE]BOJ 10950

---
+++ BOJ/20180814/10950.cpp
@@ -0,0 +1,20 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	int testcase;
+	cin >> testcase;
+	int *arr = new int[testcase];
+	for (int i = 0;i < testcase;i++)
+	{
+		cin >> a >> b;
+		arr[i] = a + b;
+	}
+	
+	for (int i = 0;i < testcase;i++)
+	{
+		cout << arr[i]<<endl;
+	}
+	return 0;
+}*/
