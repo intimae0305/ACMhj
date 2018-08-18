6f15d8c7e9f7e0c3612c2ab6726c06c5d7b30da6
intimate0305 <intimate0305@naver.com>
2018-08-18 13:12:04 +0900

    [UPDATE]BOJ 10953

---
+++ BOJ/20180814/10953.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int testcase, a, b;
+//	cin >> testcase;
+//	char **arr = new char*[testcase];
+//	for (int i = 0;i < testcase;i++)
+//	{
+//		arr[i] = new char[3];
+//		cin >> arr[i];
+//	}
+//	for (int i = 0;i < testcase;i++)
+//	{
+//		int a = (int)(arr[i][0]-'0');
+//		int b = (int)(arr[i][2]-'0');
+//		cout << a + b<<endl;
+//	}
+//	return 0;
+//}
