0a57c876d3ce4d5bf2cba28f024c85a64c13ea0c
intimate0305 <intimate0305@naver.com>
2018-08-18 14:29:33 +0900

    [UPATE]BOJ 11720

---
+++ BOJ/20180814/11720.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//int main() 
+//{
+//	int N;
+//	int n;
+//	int i;
+//	int d;
+//	int sum = 0;
+//	scanf("%d", &N);  
+//	getchar();        
+//	for (i = 0; i < N; i++)
+//	{
+//		n = getchar();
+//		d = n - '0';
+//		sum += d;
+//	}
+//	printf("%d", sum); 
+//
+//}
