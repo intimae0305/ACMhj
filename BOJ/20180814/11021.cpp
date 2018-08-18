59d3ec05d6578cf9f73f5f372c78e7921e5d025d
intimate0305 <intimate0305@naver.com>
2018-08-18 13:25:33 +0900

    [UPDATE]BOJ 11021

---
+++ BOJ/20180814/11021.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int testcase;
+//	cin >> testcase;
+//	int a, b;
+//	int *arr = new int[testcase];
+//	for (int i = 0;i < testcase;i++)
+//	{
+//		cin >> a >> b;
+//		arr[i] = a + b;
+//	}
+//	for (int i = 0;i < testcase;i++)
+//	{
+//		cout <<"Case #"<<i+1<<": "<<arr[i] << endl;
+//	}
+//	return 0;
+//}
