4fe4eab77d2c229dc70a142225197119f70bd07b
intimate0305 <intimate0305@naver.com>
2018-08-18 14:34:10 +0900

    [UPDATE]BOJ 11721

---
+++ BOJ/20180814/11721.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char arr[100];
+//	cin.getline(arr, 100);
+//	int check = 0;
+//	for (int i = 0;i < 100;i++)
+//	{
+//		if (arr[i] == '\0')
+//			break;
+//		cout << arr[i];
+//		check += 1;
+//		if (check == 10)
+//		{
+//			cout << endl;
+//			check = 0;
+//		}
+//	}
+//}
