ffe433fa017aab8ba5a9c15c96ebae0c4534742c
intimate0305 <intimate0305@naver.com>
2018-08-18 15:20:42 +0900

    [UPDATE]BOJ 10818

---
+++ BOJ/20180814/10818.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int *arr = new int[n];
+//	int max, min;
+//
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	max = arr[0];
+//	min = arr[0];
+//	for (int i = 0;i < n;i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//		if (arr[i] < min)
+//			min = arr[i];
+//	}
+//	cout << min << ' ' << max;
+//	return 0;
+//}
