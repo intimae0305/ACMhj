b0307a50a638f698f43cbca17cc43679b5b37e8c
intimate0305 <intimate0305@naver.com>
2018-08-18 15:26:30 +0900

    [UPDATE]BOJ 2438

---
+++ BOJ/20180814/2438.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	for (int j = 0;j < n;j++)
+//	{
+//
+//	for (int i = 0;i <=j;i++)
+//		{
+//			cout << '*';
+//		}
+//	cout << endl;
+//	}
+//	return 0;
+//}
