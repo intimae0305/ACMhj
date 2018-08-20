2db59885d09e609ed0cf323ef0bb6b967dfed131
intimate0305 <intimate0305@naver.com>
2018-08-20 17:47:17 +0900

    [UPDATE]BOJ 10989

---
+++ BOJ/20180814/10989.cpp
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<int> a(10000,0);
+//	for (int i = 0;i < n;i++)
+//	{
+//		int temp;
+//		scanf("%d", &temp);
+//		a[temp - 1]++;
+//	}
+//	for (int i = 0;i < 10000;i++)
+//	{
+//		for(int j=0;j<a[i];j++)
+//			printf("%d\n", i+1);
+//	}
+//	return 0;
+//}
