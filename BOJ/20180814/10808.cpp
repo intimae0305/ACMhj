433d5856f80c54e1892cdf1e2c41c3f6272cf908
intimate0305 <intimate0305@naver.com>
2018-08-21 07:18:56 +0900

    [UPDATE]BOJ 10808

---
+++ BOJ/20180814/10808.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	vector<int> a(26,0);
+//	string word;
+//	cin >> word;
+//	for (int i = 0;i < word.length();i++)
+//	{
+//		a[(int)word[i] - 97]++;
+//	}
+//	vector<int>::iterator iter;
+//	for (iter = a.begin();iter != a.end();iter++)
+//	{
+//		cout << *iter << endl;
+//	}
+//	return 0;
+//}
