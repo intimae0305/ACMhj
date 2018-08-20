e00f79b20e19c14802d4df4adc1291899a606b67
intimate0305 <intimate0305@naver.com>
2018-08-21 08:54:56 +0900

    [UPDATE]BOJ 10824

---
+++ BOJ/20180814/10824.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	string a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	long long x = stoll(a + b) + stoll(c + d);
+//	cout << x;
+//	return 0;
+//}
