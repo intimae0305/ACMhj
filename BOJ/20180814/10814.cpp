e3ce6df1b237dfa813c68d76b449f614ab50e41d
intimate0305 <intimate0305@naver.com>
2018-08-20 17:07:35 +0900

    [UPDATE]BOJ 10814

---
+++ BOJ/20180814/10814.cpp
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//struct User
+//{
+//	int age;
+//	string name;
+//};
+//bool cmp(const User& x, const User& y)
+//{
+//	return x.age < y.age;
+//}
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<User> a(n);
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> a[i].age >> a[i].name;
+//	}
+//	stable_sort(a.begin(), a.end(), cmp);
+//	for (int i = 0;i < n;i++)
+//	{
+//		printf("%d %s\n", a[i].age, a[i].name.c_str());
+//	}
+//	return 0;
+//}
--- BOJ/20180814/11651.cpp
+++ BOJ/20180814/11651.cpp
@@ -1,21 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int main()
-{
-	int n;
-	cin >> n;
-	vector<pair<int, int>> a(n);
-	for (int i = 0;i < n;i++)
-	{
-		scanf("%d%d", &a[i].second, &a[i].first);
-	}
-	sort(a.begin(), a.end());
-	for (int i = 0;i < n;i++)
-	{
-		printf("%d %d\n", a[i].second, a[i].first);
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<pair<int, int>> a(n);
+//	for (int i = 0;i < n;i++)
+//	{
+//		scanf("%d%d", &a[i].second, &a[i].first);
+//	}
+//	sort(a.begin(), a.end());
+//	for (int i = 0;i < n;i++)
+//	{
+//		printf("%d %d\n", a[i].second, a[i].first);
+//	}
+//	return 0;
+//}
