fca12232bc439b4a92ea95bd8860b8af81b53035
intimate0305 <intimate0305@naver.com>
2018-08-21 07:29:09 +0900

    [UPDATE]BOJ 10809

---
+++ BOJ/20180814/10809.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	vector<int> a(26, -1);
+//	string word;
+//	cin >> word;
+//	for (int i = 0;i < word.length();i++)
+//	{
+//		if(a[(int)word[i] - 97]==-1)
+//			a[(int)word[i] - 97] = i;
+//	}
+//	for (int i = 0;i < 26;i++)
+//	{
+//		cout << a[i]<<' ';
+//	}
+//	return 0;
+//}
