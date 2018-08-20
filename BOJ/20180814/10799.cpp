bbda797b211a75c7ae0ad42af481524b72f8dda1
intimate0305 <intimate0305@naver.com>
2018-08-21 05:18:12 +0900

    [UPDATE]BOJ 10799

---
+++ BOJ/20180814/10799.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string a;
+//	int n = 0;
+//	vector<char> b;
+//	cin >> a;
+//	for (int i = 0;i < a.length();i++)
+//	{
+//		if (a[i] == '(')
+//		{
+//			b.push_back(a[i]);
+//		}
+//		else
+//		{
+//			b.pop_back();
+//			if (a[i-1] == '(')
+//			{
+//				n += b.size();
+//			}
+//			else
+//			{
+//				n += 1;
+//			}
+//		}
+//	}
+//	cout << n;
+//	return 0;
+//}
