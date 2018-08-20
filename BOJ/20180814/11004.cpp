aa4c8a946629cb059c6b78ca50c444d8fbb26ebc
intimate0305 <intimate0305@naver.com>
2018-08-20 18:45:28 +0900

    [UPDATE]BOJ 11004

---
+++ BOJ/20180814/11004.cpp
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	int n, index;
+//	scanf("%d%d", &n, &index);
+//	vector<int> a(n);
+//	for (int i = 0;i < n;i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	sort(a.begin(), a.end());
+//	printf("%d", a[index - 1]);
+//	return 0;
+//}
