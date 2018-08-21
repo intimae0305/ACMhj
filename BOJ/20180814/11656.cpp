c3fc5cf3d5cb9bcce7cbe110bd24203141adff73
intimate0305 <intimate0305@naver.com>
2018-08-21 09:02:04 +0900

    [UPDATE]BOJ 11656

---
+++ BOJ/20180814/11656.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string a;
+//	cin >> a;
+//	vector<string> c(a.length());
+//	int size = a.length();
+//	for (int i = 0;i < a.length();i++)
+//	{
+//		c[i] = a.substr(i, a.length());
+//	}
+//	sort(c.begin(), c.end());
+//	for (int i = 0;i < c.size();i++)
+//	{
+//		cout<<c[i]<<endl;
+//	}
+//	return 0;
+//}
