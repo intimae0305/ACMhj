72af342925357878aab632af0518c9ce58229667
intimate0305 <intimate0305@naver.com>
2018-08-21 13:00:13 +0900

    [UPDATE]BOJ 10430

---
+++ BOJ/20180814/10430.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c;
+//	cin >> a >> b >> c;
+//	cout << (a + b) % c << endl << (a%c + b % c) % c << endl << (a*b) % c << endl << (a%c * b % c) % c;
+//	return 0;
+//}
