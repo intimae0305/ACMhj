c265146af18849ad6575e1c0a1834895938a540b
intimate0305 <intimate0305@naver.com>
2018-08-21 13:10:12 +0900

    [UPDATE]BOJ 2609

---
+++ BOJ/20180814/2609.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	int a, b;
+//	cin >> a >> b;
+//	int m=min(a, b);
+//	int c = 0;
+//	for (int i = 1;i <= m;i++)
+//	{
+//		if (a%i == 0 && b%i == 0 && c < i)
+//			c = i;
+//	}
+//	cout << c << endl << a / c * b;
+//
+//	return 0;
+//}
