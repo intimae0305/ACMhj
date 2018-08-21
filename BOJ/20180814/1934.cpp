05cbf47ca3f0d451db21e9e9bf9a1dbf1397d6b1
intimate0305 <intimate0305@naver.com>
2018-08-21 13:27:28 +0900

    [UPDATE]BOJ 1934

---
+++ BOJ/20180814/1934.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	int n,a, b;
+//	int find = 0;
+//	cin >> n;
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> a >> b;
+//		int m = min(a, b);
+//		for (int j = 1;j <= m;j++)
+//		{
+//			if (find < j&& a%j == 0 && b%j == 0)
+//				find = j;
+//		}
+//		cout << a / find * b << endl;
+//		find = 0;
+//	}
+//	return 0;
+//}
