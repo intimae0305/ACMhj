0644bcfc2f8da0d8756377dd8365c0994e4e1226
intimate0305 <intimate0305@naver.com>
2018-08-21 10:58:56 +0900

    [UPDATE]BOJ 1158

---
+++ BOJ/20180814/1158.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <queue>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//	queue<int> u;
+//	cout << '<';
+//	for (int i = 0;i < n;i++)
+//	{
+//		u.push(i + 1);
+//	}
+//	while (!u.empty())
+//	{
+//		for (int i = 0;i < m-1;i++)
+//		{
+//			u.push(u.front());
+//			u.pop();
+//		}
+//		cout << u.front();
+//		u.pop();
+//		if (!u.empty())
+//		{
+//			cout << ", ";
+//		}
+//	}
+//	cout << '>';
+//	return 0;
+//}
