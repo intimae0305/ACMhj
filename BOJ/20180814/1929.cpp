0453c51fe020bbfeaad263f48c3a7df4b18dc1f8
intimate0305 <intimate0305@naver.com>
2018-08-23 16:03:29 +0900

    [UPDATE]BOJ 1929

--- BOJ/20180814/1929.cpp
+++ BOJ/20180814/1929.cpp
@@ -7,5 +7,24 @@ int main()
 {
 	int m, n;
 	cin >> m >> n;
+	int *arr=new int[1000001];
+	for (int i = 2;i < 1000001;i++)
+	{
+		arr[i] = 1;
+	}
+	arr[1] = 0;
+	arr[0] = 0;
+	for (int i = 2;i <= n;i++)
+	{
+		for (int j = 2;i*j <= n;j++)
+		{
+			arr[i*j] = 0;
+		}
+	}
+	for (int i = m;i <= n;i++)
+	{
+		if (arr[i] == 1)
+			cout << i<< ' ';
+	}
 	return 0;
 }
